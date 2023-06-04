2d8d162b7778326d4f570fd687b7333fc9cd4f77
Tejv1708 <narukatejveer@gmail.com>
2023-06-04 14:34:22 +0530

    seconde commit

--- Assignment.js
+++ Assignment.js
@@ -1,41 +1,56 @@
-const puppeteer = require('puppeteer');
+
+import { launch } from 'puppeteer';
 
 async function runTest() {
   // Launch a headless browser
-  const browser = await puppeteer.launch();
+  const browser = await launch();
   const page = await browser.newPage();
 
   try {
     // Go to Google Flights website
     await page.goto('https://www.google.com/flights');
 
+    // Wait for the input field to appear
+    await page.waitForSelector('input[placeholder="Where from?"]');
+    
     // Enter the origin and destination airports
-    await page.type('input[name="origin"]', 'Delhi');
-    await page.type('input[name="destination"]', 'Jaipur');
+    await page.type('input[placeholder="Where from?"]', 'Delhi');
+    await page.type('input[placeholder="Where to?"]', 'Jaipur');
 
     // Select the departure date
-    await page.click('input[aria-label="Departure date"]');
-    await page.click('div[data-day="2023-04-15"]');
+    await page.click('input[aria-label = "Departure"]');
+    await page.keyboard.type('Date ➝ 12 April 2023');
 
     // Click the search button
-    await page.click('button[aria-label="Search"]');
-
-    // Wait for the flight results to load
-    await page.waitForSelector('div[data-flt-ve="flt"]');
-
-    // Get the prices of the flights
-    const prices = await page.$$eval('div[data-flt-ve="flt"]', (elements) => {
-      const flightPrices = {};
-
-      elements.forEach((el) => {
-        const airline = el.querySelector('div.gws-flights-results__carriers').textContent.trim();
-        const price = el.querySelector('span.IHreh').textContent.trim();
-
-        flightPrices[airline] = price;
+    await page.evaluate(() => {
+        const elements = Array.from(document.querySelectorAll('span'));
+        const exploreElement = elements.find(element => element.textContent.includes('Explore'));
+        if (exploreElement) {
+          exploreElement.click();
+        }
       });
 
-      return flightPrices;
-    });
+    // Wait for the flight results to load
+
+    await page.waitForSelector('body');
+
+    
+
+    // Get the prices of the flights
+    const prices = await page.$$eval('ul.Rk10dc', (elements) => {
+        const flightPrices = {};
+        elements.forEach((el) => {
+          const airline = el.querySelector('div.sSHqwe tPgKwe ogfYpf');
+          const price = el.querySelector('div.U3gSDe');
+      
+          if (airline && price) {
+            flightPrices[airline.textContent.trim()] = price.textContent.trim();
+          }
+        });
+      
+        return flightPrices;
+      });
+      
 
     // Print the prices
     console.log('Flight Prices:');
@@ -49,3 +64,4 @@ async function runTest() {
 }
 
 runTest();
+
